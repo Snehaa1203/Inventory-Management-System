8d42b5d4923109b6929874e3e1639868b03808f9
Sneha <snehasachdeva130@gmail.com>
2021-08-02 18:13:53 +0530

    forget password added

--- inventory/inventory/urls.py
+++ inventory/inventory/urls.py
@@ -26,8 +26,13 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
-     path('profile/',views.profile,name='profile'),
-     path('profile/update',views.profile_update,name='profile_update'),
+    path('profile/',views.profile,name='profile'),
+    path('profile/update',views.profile_update,name='profile_update'),
+    path('password_reset',auth_views.PasswordResetView.as_view(),name="password_reset"),
+    path("password_reset_done",auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path("password_reset_confirm/<uidb64>/<token>",auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path("password_reset_complete/",auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+     
 
 
 ]
